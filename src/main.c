c8481c00576cdb95583e89c38a0a13c9d9a3f774
eduardshindel <shindeleduard10@gmail.com>
2019-06-04 09:43:37 +0700

    Bug fix

--- src/main.c
+++ src/main.c
@@ -8,47 +8,44 @@ int main()
 {
 	int i, j;
 
-	//	int d[10000];
-	//	int prev[10000];
+	int d[10000];
+	int prev[10000];
 
-	//	struct graph *g;
-	//	g = graph_create(10000);
+	struct graph *g;
+	g = graph_create(10000);
+	i = 1;
+	while (i < 10000)
+	{
+		if (!(i % 100))
+		{
+			graph_set_edge(g, i, i + 100, 30);
+		}
+		else if (i > 9900)
+		{
+			graph_set_edge(g, i, i + 1, 30);
+		}
+		else
+		{
+			graph_set_edge(g, i, i + 1, 30);
+			graph_set_edge(g, i, i + 100, 30);
+		}
+		i++;
+	}
+	ShortestPathDijkstra(g, 1, d, prev);
+	for (i = 1; i <= g->nvertices; i++)
+	{
+	printf(" %d\t", d[i]);
+	}
+	printf("\n");
 
-	//	i = 1;
-	//	while (i < 10000)
-	//	{
-	//		if (!(i % 100))
-	//		{
-	//			graph_set_edge(g, i, i + 100, 30);
-	//		}
-	//		else if (i > 9900)
-	//		{
-	//			graph_set_edge(g, i, i + 1, 30);
-	//		}
-	//		else
-	//		{
-	//			graph_set_edge(g, i, i + 1, 30);
-	//			graph_set_edge(g, i, i + 100, 30);
-	//		}
-	//		i++;
-	//	}
-
-	//	ShortestPathDijkstra(g, 1, d, prev);
-
-	//	for (i = 1; i <= g->nvertices; i++)
-	//	{
-	//		printf(" %d\t", d[i]);
-	//	}
-	//	printf("\n");
-
-	int d1[20];
-	int prev1[20];
-	int path1[20];
+/*	int d1[100];
+	int prev1[100];
+	int path1[100];
 
 	struct graph *g1;
-	g1 = graph_create(20);
+	g1 = graph_create(100);
 
-	for (int i = 1; i < 20; i++)
+	for (int i = 1; i < 100; i++)
 	{
 		graph_set_edge(g1, i, i + 1, 6);
 	}
@@ -60,6 +57,6 @@ int main()
 		printf(" %d\t", d1[i]);
 	}
 	printf("\n");
-
+*/
 	return 0;
 }
